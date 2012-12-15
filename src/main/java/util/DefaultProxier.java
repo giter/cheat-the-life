8b83bd3713f3492e1956865bc19f5d84d7773f9c
giter <nubix@qq.com>
2012-12-15 23:06:15 +0800

    修正proxier

--- src/main/java/util/DefaultProxier.java
+++ src/main/java/util/DefaultProxier.java
@@ -19,76 +19,71 @@ import org.slf4j.LoggerFactory;
 
 public class DefaultProxier implements Proxier {
 
-	final static HttpClient						hc			= new HttpClient().connect(5000);
-	private final static String				URL;
+  final static HttpClient client = new HttpClient().connect(5000);
+  private final static String URL;
 
-	static {
-		Properties properties = new Properties();
-		try {
-			properties.load(DefaultProxier.class.getResourceAsStream("/resources.properties"));
-			URL = properties.get("proxier").toString();
-		} catch (IOException e) {
-			throw new RuntimeException("No properties specified!", e);
-		}
-	}
+  static {
+    Properties properties = new Properties();
+    try {
+      properties.load(DefaultProxier.class.getResourceAsStream("/resources.properties"));
+      URL = properties.get("proxier").toString();
+    } catch (IOException e) {
+      throw new RuntimeException("No properties specified!", e);
+    }
+  }
 
-	private static final Logger				logger	= LoggerFactory.getLogger(DefaultProxier.class);
+  private static final Logger logger = LoggerFactory.getLogger(DefaultProxier.class);
 
-	private final static Queue<Proxy>	proxies	= new ConcurrentLinkedQueue<Proxy>();
+  private final static Queue<Proxy> proxies = new ConcurrentLinkedQueue<Proxy>();
 
-	public static void fillPool() {
+  public static void fillPool() {
 
-		if (logger.isDebugEnabled()) {
-			logger.debug("Loading proxy list: " + URL);
-		}
+    if (logger.isDebugEnabled()) {
+      logger.debug("Loading proxy list: " + URL);
+    }
 
-		for (int i = 0; i < 0x7fffffff; i++) {
-			try {
+    for (int i = 0; i < 0x7fffffff; i++) {
+      try {
 
-				for (String s : hc.GET(URL).getValue().split("\r?\n")) {
-					String host = s.substring(0, s.indexOf(':'));
-					int port = NumberUtils.toInt(s.substring(s.indexOf(':') + 1));
-					SocketAddress sa = new InetSocketAddress(host, port);
-					Proxy p = new Proxy(Type.HTTP, sa);
-					proxies.add(p);
-				}
+        for (String s : client.GET(URL).getValue().split("\r?\n")) {
+          String host = s.substring(0, s.indexOf(':'));
+          int port = NumberUtils.toInt(s.substring(s.indexOf(':') + 1));
+          SocketAddress sa = new InetSocketAddress(host, port);
+          Proxy p = new Proxy(Type.HTTP, sa);
+          proxies.add(p);
+        }
 
-				if (logger.isDebugEnabled()) {
-					logger.debug(String.format("Loaded %d proxies", proxies.size()));
-				}
+        if (logger.isDebugEnabled()) {
+          logger.debug(String.format("Loaded %d proxies", proxies.size()));
+        }
 
-				try {
-					Thread.sleep(new Random().nextInt(5) * 1000);
-				} catch (InterruptedException e) {
-					throw new RuntimeException(e);
-				}
+        try {
+          Thread.sleep(new Random().nextInt(5) * 1000);
+        } catch (InterruptedException e) {
+          throw new RuntimeException(e);
+        }
 
-				return;
-			} catch (IOException e) {
-				logger.warn("Cannot access proxy at " + i + " times , try again!");
-			}
+        return;
+      } catch (IOException e) {
+        logger.warn("Cannot access proxy at " + i + " times , try again!");
+      }
 
-			try {
-				Thread.sleep(new Random().nextInt(5) * 1000);
-			} catch (InterruptedException e) {
-				throw new RuntimeException(e);
-			}
-		}
+      try {
+        Thread.sleep(new Random().nextInt(5) * 1000);
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+    }
 
-	}
+  }
 
-	@Override
-	public Proxy get() {
+  @Override
+  public synchronized Proxy get() {
 
-		while (proxies.isEmpty()) {
-			synchronized (proxies) {
-				if (proxies.isEmpty()) {
-					fillPool();
-				}
-			}
-		}
+    if (proxies.isEmpty()) {
+      fillPool();
+    }
 
-		return proxies.remove();
-
-	}
+    return proxies.remove();
+  }
 }
